6af6153bcd5dcd58bd07a2686ea9bd1492d2e828
Themadprogrammer1 <160073928+Themadprogrammer1@users.noreply.github.com>
2026-05-07 21:23:11 +0300

    very basic code

--- Assignments/Semester_B/Assignment1/ex1.py
+++ Assignments/Semester_B/Assignment1/ex1.py
@@ -5,29 +5,82 @@ import utils as utils
 id = ["No numbers - I'm special!"]
 
 
+class State:
+    def __init__(self, elevatorsPosition, personsPosition):
+        self.elevatorsPosition = tuple(elevatorsPosition)
+        self.personsPosition = tuple(personsPosition)
+
+    def __eq__(self, other):
+        return isinstance(other, State) and \
+               self.elevatorsPosition == other.elevatorsPosition and \
+               self.personsPosition == other.personsPosition
+
+    def __hash__(self):
+        return hash((self.elevatorsPosition, self.personsPosition))
+
 
 class ElevatorsProblem(search.Problem):
-    """This class implements an elevators problem"""
-
     def __init__(self, initial):
+        self.height = initial["height"]
+        self.e_ids = tuple(sorted(initial["Elevators"].keys()))
+        self.p_ids = tuple(sorted(initial["Persons"].keys()))
+        self.elevator_specs = {}
+        self.person_specs = {}
 
-        
-        """ Constructor only needs the initial state.
-        Don't forget to set the goal or implement the goal test"""
-        search.Problem.__init__(self, initial)
+        initial_elevators = []
+        for e_id in self.e_ids:
+            curr_f, reach, weight = initial["Elevators"][e_id]
+            self.elevator_specs[e_id] = {"reachable": frozenset(reach), "max_weight": weight}
+            initial_elevators.append(curr_f)
+
+        initial_persons = []
+        for p_id in self.p_ids:
+            start_f, w, goal_f = initial["Persons"][p_id]
+            self.person_specs[p_id] = {"weight": w, "goal": goal_f}
+            initial_persons.append(start_f)
+
+        search.Problem.__init__(self, State(initial_elevators, initial_persons))
 
     def successor(self, state):
-        """ Generates the successor states returns [(action, achieved_states, ...)]"""
-        utils.raiseNotDefined()
+        successors = []
+        for i, e_id in enumerate(self.e_ids):
+            curr_f = state.elevatorsPosition[i]
+            for target_f in self.elevator_specs[e_id]["reachable"]:
+                if target_f != curr_f:
+                    new_e_pos = list(state.elevatorsPosition)
+                    new_e_pos[i] = target_f
+                    successors.append((f"MOVE{{{e_id},{target_f}}}", State(new_e_pos, state.personsPosition)))
+        for i, p_id in enumerate(self.p_ids):
+            p_loc = state.personsPosition[i]
+            p_weight = self.person_specs[p_id]["weight"]
+            if isinstance(p_loc, int):
+                for j, e_id in enumerate(self.e_ids):
+                    if state.elevatorsPosition[j] == p_loc:
+                        e_max = self.elevator_specs[e_id]["max_weight"]
+                        curr_w = sum(self.person_specs[self.p_ids[k]]["weight"] 
+                                     for k, loc in enumerate(state.personsPosition) 
+                                     if loc == f"e{e_id}")
+                        if curr_w + p_weight <= e_max:
+                            new_p_pos = list(state.personsPosition)
+                            new_p_pos[i] = f"e{e_id}"
+                            successors.append((f"ENTER{{{p_id},{e_id}}}", State(state.elevatorsPosition, new_p_pos)))
+            else:
+                e_id = int(p_loc[1:])
+                e_idx = self.e_ids.index(e_id)
+                curr_f = state.elevatorsPosition[e_idx]
+                new_p_pos = list(state.personsPosition)
+                new_p_pos[i] = curr_f
+                successors.append((f"EXIT{{{p_id},{e_id}}}", State(state.elevatorsPosition, new_p_pos)))
+        return successors
 
     def goal_test(self, state):
-        """ given a state, checks if this is the goal state, compares to the created goal state returns True/False"""
-        utils.raiseNotDefined()
+        for i, p_id in enumerate(self.p_ids):
+            if state.personsPosition[i] != self.person_specs[p_id]["goal"]:
+                return False
+        return True
 
     def h_astar(self, node):
-        """ This is the heuristic. It gets a node (not a state)
-        and returns a goal distance estimate"""
-        utils.raiseNotDefined()
+        return 0
 
 
 def create_elevators_problem(game):
